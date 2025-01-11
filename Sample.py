d5376befc9fe4ea90f3c881e1c48d29c12bd17f8
PeirongShi <shipr1003@gmail.com>
2025-01-11 16:46:25 +0100

    Update Sample.py

--- Sample.py
+++ Sample.py
@@ -5,40 +5,40 @@ import csv
 # Data to be written in the CSV file
 data = [
     {
-        "OriginA": "5.373588,-3.998759",
-        "DestinationA": "5.327810,-4.005012",
-        "OriginB": "5.361826,-3.990009",
-        "DestinationB": "5.322763,-4.002270",
+        "home_A": "5.373588,-3.998759",
+        "work_A": "5.327810,-4.005012",
+        "home_B": "5.361826,-3.990009",
+        "work_B": "5.322763,-4.002270",
     },
     {
-        "OriginA": "5.373588,-3.998760",
-        "DestinationA": "5.327810,-4.005013",
-        "OriginB": "5.368385,-4.006019",
-        "DestinationB": "5.335087,-3.995491",
+        "home_A": "5.373588,-3.998760",
+        "work_A": "5.327810,-4.005013",
+        "home_B": "5.368385,-4.006019",
+        "work_B": "5.335087,-3.995491",
     },
     {
-        "OriginA": "5.373588,-3.998761",
-        "DestinationA": "5.327810,-4.005014",
-        "OriginB": "5.355748,-3.969820",
-        "DestinationB": "5.333238,-4.006999",
+        "home_A": "5.373588,-3.998761",
+        "work_A": "5.327810,-4.005014",
+        "home_B": "5.355748,-3.969820",
+        "work_B": "5.333238,-4.006999",
     },
     {
-        "OriginA": "5.373588,-3.998762",
-        "DestinationA": "5.327810,-4.005015",
-        "OriginB": "5.392951,-3.975507",
-        "DestinationB": "5.347369,-4.003102",
+        "home_A": "5.373588,-3.998762",
+        "work_A": "5.327810,-4.005015",
+        "home_B": "5.392951,-3.975507",
+        "work_B": "5.347369,-4.003102",
     },
     {
-        "OriginA": "5.361826,-3.990009",
-        "DestinationA": "5.322763,-4.002270",
-        "OriginB": "5.368385,-4.006019",
-        "DestinationB": "5.335087,-3.995491",
+        "home_A": "5.361826,-3.990009",
+        "work_A": "5.322763,-4.002270",
+        "home_B": "5.368385,-4.006019",
+        "work_B": "5.335087,-3.995491",
     },
     {
-        "OriginA": "5.355748,-3.969820",
-        "DestinationA": "5.333238,-4.006999",
-        "OriginB": "5.392951,-3.975507",
-        "DestinationB": "5.347369,-4.003102",
+        "home_A": "5.355748,-3.969820",
+        "work_A": "5.333238,-4.006999",
+        "home_B": "5.392951,-3.975507",
+        "work_B": "5.347369,-4.003102",
     },    
 ]
 
@@ -48,7 +48,7 @@ file_path = "origin_destination_coordinates.csv"
 # Write the data to a CSV file
 with open(file_path, mode='w', newline='') as file:
     writer = csv.DictWriter(
-        file, fieldnames=["OriginA", "DestinationA", "OriginB", "DestinationB"]
+        file, fieldnames=["home_A", "work_A", "home_B", "work_B"]
     )
     writer.writeheader()  # Write the header
     writer.writerows(data)  # Write the rows
