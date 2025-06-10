0f30b98e738a458020249878c1235ebf769ecd90
PeirongShi <shipr1003@gmail.com>
2025-06-10 14:36:32 +0200

    update

--- CanterburyCommuto/canterburycommuto/Sample.py
+++ CanterburyCommuto/canterburycommuto/Sample.py
@@ -5,47 +5,54 @@ import csv
 # Data to be written in the CSV file
 data = [
     {
-        "home_A": "5.373588,-3.998759",
-        "work_A": "5.327810,-4.005012",
-        "home_B": "5.373588,-3.998759",
-        "work_B": "5.327810,-4.005012",
+        "ID": "R1",
+        "home_A_latitude": "5.373588", "home_A_longitude": "-3.998759",
+        "work_A_latitude": "5.327810", "work_A_longitude": "-4.005012",
+        "home_B_latitude": "5.373588", "home_B_longitude": "-3.998759",
+        "work_B_latitude": "5.327810", "work_B_longitude": "-4.005012",
     },
     {
-        "home_A": "5.373588,-3.998759",
-        "work_A": "5.327810,-4.005012",
-        "home_B": "5.361826,-3.990009",
-        "work_B": "5.322763,-4.002270",
+        "ID": "R2",
+        "home_A_latitude": "5.373588", "home_A_longitude": "-3.998759",
+        "work_A_latitude": "5.327810", "work_A_longitude": "-4.005012",
+        "home_B_latitude": "5.361826", "home_B_longitude": "-3.990009",
+        "work_B_latitude": "5.322763", "work_B_longitude": "-4.002270",
     },
     {
-        "home_A": "5.373588,-3.998760",
-        "work_A": "5.327810,-4.005013",
-        "home_B": "5.368385,-4.006019",
-        "work_B": "5.335087,-3.995491",
+        "ID": "R3",
+        "home_A_latitude": "5.373588", "home_A_longitude": "-3.998760",
+        "work_A_latitude": "5.327810", "work_A_longitude": "-4.005013",
+        "home_B_latitude": "5.368385", "home_B_longitude": "-4.006019",
+        "work_B_latitude": "5.335087", "work_B_longitude": "-3.995491",
     },
     {
-        "home_A": "5.373588,-3.998761",
-        "work_A": "5.327810,-4.005014",
-        "home_B": "5.355748,-3.969820",
-        "work_B": "5.333238,-4.006999",
+        "ID": "R4",
+        "home_A_latitude": "5.373588", "home_A_longitude": "-3.998761",
+        "work_A_latitude": "5.327810", "work_A_longitude": "-4.005014",
+        "home_B_latitude": "5.355748", "home_B_longitude": "-3.969820",
+        "work_B_latitude": "5.333238", "work_B_longitude": "-4.006999",
     },
     {
-        "home_A": "5.373588,-3.998762",
-        "work_A": "5.327810,-4.005015",
-        "home_B": "5.392951,-3.975507",
-        "work_B": "5.347369,-4.003102",
+        "ID": "R5",
+        "home_A_latitude": "5.373588", "home_A_longitude": "-3.998762",
+        "work_A_latitude": "5.327810", "work_A_longitude": "-4.005015",
+        "home_B_latitude": "5.392951", "home_B_longitude": "-3.975507",
+        "work_B_latitude": "5.347369", "work_B_longitude": "-4.003102",
     },
     {
-        "home_A": "5.361826,-3.990009",
-        "work_A": "5.322763,-4.002270",
-        "home_B": "5.368385,-4.006019",
-        "work_B": "5.335087,-3.995491",
+        "ID": "R6",
+        "home_A_latitude": "5.361826", "home_A_longitude": "-3.990009",
+        "work_A_latitude": "5.322763", "work_A_longitude": "-4.002270",
+        "home_B_latitude": "5.368385", "home_B_longitude": "-4.006019",
+        "work_B_latitude": "5.335087", "work_B_longitude": "-3.995491",
     },
     {
-        "home_A": "5.355748,-3.969820",
-        "work_A": "5.333238,-4.006999",
-        "home_B": "5.392951,-3.975507",
-        "work_B": "5.347369,-4.003102",
-    },    
+        "ID": "R7",
+        "home_A_latitude": "5.355748", "home_A_longitude": "-3.969820",
+        "work_A_latitude": "5.333238", "work_A_longitude": "-4.006999",
+        "home_B_latitude": "5.392951", "home_B_longitude": "-3.975507",
+        "work_B_latitude": "5.347369", "work_B_longitude": "-4.003102",
+    },
 ]
 
 # Path to save the CSV file
@@ -54,9 +61,12 @@ file_path = "origin_destination_coordinates.csv"
 # Write the data to a CSV file
 with open(file_path, mode='w', newline='') as file:
     writer = csv.DictWriter(
-        file, fieldnames=["home_A", "work_A", "home_B", "work_B"]
+        file, fieldnames = ["ID", "home_A_latitude", "home_A_longitude", "work_A_latitude", "work_A_longitude", "home_B_latitude", "home_B_longitude", "work_B_latitude", "work_B_longitude"]
     )
     writer.writeheader()  # Write the header
     writer.writerows(data)  # Write the rows
 
 print(f"CSV file saved as {file_path}")
+
+
+
